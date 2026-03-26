bfe4d00c1553f29e199378d5c1f3033ec112d4bf
kidphungnoi-oss <kid.phungnoi@gmail.com>
2026-03-26 17:52:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+// test หน้าเว็บ
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
+// webhook
 app.post('/webhook', (req, res) => {
-  console.log('WEBHOOK:', req.body);
+  console.log('Webhook received');
   res.status(200).send('OK');
 });
 
+// run server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server running on port ' + port);
